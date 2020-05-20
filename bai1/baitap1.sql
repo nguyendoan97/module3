c09f462e2b976c3da7b22bd224089f1e92b8be1c
doancodegym <nguyenhongdoan14@gmailcom>
2020-05-20 17:18:24 +0700

    bai tap dau tien

---
+++ bai1/baitap1.sql
@@ -0,0 +1,12 @@
+create database baitap;
+use baitap;
+CREATE TABLE Persons (
+                         PersonID int,
+                         LastName varchar(255),
+                         FirstName varchar(255),
+                         Address varchar(255),
+                         City varchar(255)
+);
+insert into Persons(PersonID, LastName, FirstName, Address, City) values ('3','Lê','Hiếu','TP Vinh','Nghệ An');
+
+select * from Persons;
